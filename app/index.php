ade98191f62369088799a3c6ce238ba0ac45dcd2
Riina K <riinak.tv@gmail.com>
2021-12-18 21:04:02 +0900

    ログに出力されるタイムゾーンを変更

--- app/index.php
+++ app/index.php
@@ -13,6 +13,17 @@ switch ($path) {
         phpinfo();
         break;
 
+    case '/time':
+        echo date(DATE_RFC2822);
+        break;
+
+    case '/error':
+        // エラーログを確認できるよう、強制的にエラーを発動させる
+        header("HTTP/1.1 500 Internal Server Error");
+        trigger_error("Test error.", E_USER_ERROR);
+        break;
+
     default:
+        header("HTTP/1.1 404 Not Found");
         throw new Exception("Unknown route '{$path}'");
 }

eb0108238a8f365294777897ff95780966d476d9
Riina K <riinak.tv@gmail.com>
2021-12-19 00:06:57 +0900

    Docker の mariadb に接続成功

--- project/app/src/Route.php
+++ project/app/src/Route.php
@@ -53,10 +53,19 @@ class Route
 
     /**
      * "/db" で呼ばれた場合、データベースへの接続を試みる
+     * 接続が確立されたら、サーバ内の DB 一覧を取得してみる
      */
     public function db(): void
     {
-        new PDO('mysql:dbname=docker_example;host=127.0.0.1', 'user_example', 'any_password');
+        // ホスト名には docker-compose.yml で定義したコンテナ名が使用できる
+        // docker-compose に書いた「13306」ポートは外向きのものなので、コンテナ内部では利用できない
+        $pdo = new PDO('mysql:dbname=db_docker_example;host=example_mysql;port=3306', 'user_example', 'any_password');
+        $statement = $pdo->query('SHOW DATABASES;');
+
+        foreach ($statement as $row) {
+            var_dump($row);
+            echo "<br />\n";
+        }
     }
 
     /**
